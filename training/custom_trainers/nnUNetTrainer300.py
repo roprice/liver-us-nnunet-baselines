4187484475bb525594684780a330db6c6f2ce2f2
Ro <rowan@remaphq.com>
2026-08-11 13:05:19 -0700

    updated to avoid ref to init

--- training/custom_trainers/nnUNetTrainer300.py
+++ training/custom_trainers/nnUNetTrainer300.py
@@ -2,13 +2,7 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
 
 class nnUNetTrainer300(nnUNetTrainer):
-    """nnU-Net trainer limited to 300 epochs (default is 1000).
-
-    Used for ablation experiments comparing epoch budgets.
-    """
-    def __init__(self, plans: dict, configuration: str, fold: int,
-                 dataset_json: dict, unpack_dataset: bool = True,
-                 device=None):
-        super().__init__(plans, configuration, fold, dataset_json,
-                         unpack_dataset, device)
+    """nnU-Net trainer limited to 300 epochs (default is 1000)."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.num_epochs = 300
